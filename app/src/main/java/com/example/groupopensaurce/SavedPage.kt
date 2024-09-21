9c2d489214e52339eb044613b6ae45e91be48d73
Thebi-debug <st10169116@vcconnect.edu.za>
2024-09-21 23:52:54 +0200

    user profile

---
+++ app/src/main/java/com/example/groupopensaurce/SavedPage.kt
@@ -0,0 +1,11 @@
+package com.example.groupopensaurce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SavedPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved_page)
+    }
+}
